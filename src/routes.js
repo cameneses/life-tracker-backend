d8219a12d6d14411a8b4769ee0222314524d95fd
Cesar Meneses <cnmeneses@uc.cl>
2019-12-23 21:44:46 -0300

    add user routes to app router

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 const KoaRouter = require('koa-router');
 
+const userRoutes = require('./routes/user');
+
 const router = new KoaRouter();
+
+router.use('/users', userRoutes.routes());
 
 module.exports = router;
