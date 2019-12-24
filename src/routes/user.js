f4a0a43fcc51d44b8aae5da324d76cabdfb69fa4
Cesar Meneses <cnmeneses@uc.cl>
2019-12-23 21:44:31 -0300

    define login and signup user routes

---
+++ src/routes/user.js
@@ -0,0 +1,24 @@
+const KoaRouter = require("koa-router");
+const jwt = require("jsonwebtoken");
+
+const router = new KoaRouter();
+
+router.post("user.signup", "/signup", async ctx => {
+  const { username, email, password } = ctx.request.body;
+  const user = await ctx.orm.user.create({ username, email, password });
+  ctx.body = { user };
+});
+
+router.post("user.login", "/login", async ctx => {
+  const { email, password } = ctx.request.body;
+  const user = await ctx.orm.user.findOne({ where: { email } });
+  if (user && (await user.checkPassword(password))) {
+    const token = await jwt.sign({ user }, process.env.JWT_SECRET);
+    ctx.body = { token };
+  } else {
+    ctx.throw(401, "Wrong email or password");
+  }
+});
+
+module.exports = router;
+
