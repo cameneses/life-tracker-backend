6d97da23bafa0977ce9762c7946765dcb33d57a9
Cesar Meneses <cnmeneses@uc.cl>
2019-12-23 23:28:34 -0300

    add aliment and category to main router

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
-const KoaRouter = require('koa-router');
+const KoaRouter = require("koa-router");
 
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
+const alimentRoutes = require("./routes/aliment");
+const categoryRoutes = require("./routes/category");
 
 const router = new KoaRouter();
 
-router.use('/users', userRoutes.routes());
+// Unauthenticated routes
+router.use("/users", userRoutes.routes());
+
+// Authenticated routes
+router.use("/aliments", alimentRoutes.routes());
+
+// Admin routes: soon
+router.use("/categories", categoryRoutes.routes());
 
 module.exports = router;
