4e2e4c5cd8c0582520f1c02fdadd0540c10fe67f
Cesar Meneses <cnmeneses@uc.cl>
2019-12-24 00:24:59 -0300

    add food routes to app router

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const KoaRouter = require("koa-router");
 const userRoutes = require("./routes/user");
 const alimentRoutes = require("./routes/aliment");
 const categoryRoutes = require("./routes/category");
+const foodRoutes = require("./routes/food");
 
 const router = new KoaRouter();
 
@@ -11,6 +12,7 @@ router.use("/users", userRoutes.routes());
 
 // Authenticated routes
 router.use("/aliments", alimentRoutes.routes());
+router.use("/food", foodRoutes.routes());
 
 // Admin routes: soon
 router.use("/categories", categoryRoutes.routes());
