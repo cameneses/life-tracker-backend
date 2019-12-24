fd63e5d3937f037e90175753d1d3ce710dcbcee1
Cesar Meneses <cnmeneses@uc.cl>
2019-12-23 23:27:21 -0300

    define routes for categories

---
+++ src/routes/category.js
@@ -0,0 +1,33 @@
+const KoaRouter = require("koa-router");
+
+const router = new KoaRouter();
+
+router.post("category.create", "/", async ctx => {
+  const { name } = ctx.request.body;
+  try {
+    const category = await ctx.orm.category.create({ name });
+    ctx.body = { category };
+  } catch (validationError) {
+    ctx.throw(500, "Validation error");
+  }
+});
+
+router.get("category.all", "/", async ctx => {
+  const categories = await ctx.orm.category.findAll();
+  ctx.body = { categories };
+});
+
+router.get("category.aliments", "/:name", async ctx => {
+  const { name } = ctx.params;
+  try {
+    const category = await ctx.orm.category.findOne({
+      where: { name },
+      include: [{ model: ctx.orm.aliment }]
+    });
+    ctx.body = { category };
+  } catch (validationError) {
+    ctx.throw(500, "Validation error");
+  }
+});
+
+module.exports = router;
